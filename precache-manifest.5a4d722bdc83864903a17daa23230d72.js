a0dbe58ccf7de9cc7ccbaf909791093445e11729
jayse45 <ngyasi0@gmail.com>
2022-04-18 20:30:34 +0000

    Updates

---
+++ precache-manifest.5a4d722bdc83864903a17daa23230d72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bba1fdf70ebac07d8649",
+    "url": "/cv-react_project/static/css/main.f8223b9f.chunk.css"
+  },
+  {
+    "revision": "bba1fdf70ebac07d8649",
+    "url": "/cv-react_project/static/js/main.bba1fdf7.chunk.js"
+  },
+  {
+    "revision": "75e26edd0ff8491a1455",
+    "url": "/cv-react_project/static/js/1.75e26edd.chunk.js"
+  },
+  {
+    "revision": "42ad502913df65e4e428",
+    "url": "/cv-react_project/static/js/runtime~main.42ad5029.js"
+  },
+  {
+    "revision": "c410d7e2822fce55d1db81a155ba4163",
+    "url": "/cv-react_project/index.html"
+  }
+];
